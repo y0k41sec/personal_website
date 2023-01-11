4451a7c525cf1609c3f0e59c959eb2495e889fec
Jonathan Goya Nogiri <jonathannogiri@gmail.com>
2023-01-11 20:49:18 +0000

    first commit

---
+++ app/root.jsx
@@ -0,0 +1,34 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+      <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <script src="https://cdn.tailwindcss.com"></script>
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.jsx
@@ -0,0 +1,38 @@
+export default function Index() {
+  return (
+    <div class="p-8 flex flex-col items-center mx-auto max-w-2xl">
+      <img
+        class="w-16 h-16 rounded-full"
+        src="/shibe.jpeg"
+      />
+
+      <h1 class="my-4 text-2xl font-bold">Jojo - y0k41sec</h1>
+
+      <p class="my-4 text-lg">
+        I'm Jonathan Nogiri, a security engineer who loves to learn technology.
+      </p>
+
+      <div class="my-4 flex text-center w-1/2">
+        <a class="flex-1 font-semibold" href="https://www.linkedin.com/in/jonathan-n-2852b028/?locale=en_US">
+          Linkedin
+        </a>
+
+        <a class="flex-1 font-semibold" href="https://github.com/y0k41sec">
+          GitHub
+        </a>
+      </div>
+
+      <ul class="my-8 text-center w-2/3 mx-auto space-y-4">
+        <li class="p-4 w-full rounded border transition-all hover:bg-orange-600 hover:text-white">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/jonathan-n-2852b028/?locale=en_US"
+          >
+            Connect with me on Linkedin
+          </a>
+        </li>
+
+      </ul>
+    </div>
+  );
+}
